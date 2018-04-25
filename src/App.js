fe19539a518dae6e542b76dbb8aa2e55f62d18b0
ali1129js <ali1129js@gmail.com>
2018-04-25 15:05:41 +0200

    First we add the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 /**
  * @Author: Ali Ismail
  * @Date:   2018-04-25T14:58:39+02:00
- * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-25T14:58:39+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-04-25T15:03:29+02:00
  */
-
-
-
 import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
+    <Router>
 
+    </Router>
     )
   }
 }
